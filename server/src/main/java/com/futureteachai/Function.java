70469b924812150dda7ff1f20bb749f85ec817dc
Elsdon Chang <elsdonc21@gmail.com>
2023-11-17 02:27:06 -0600

    store pdf in Azure blob storage and return url

--- server/src/main/java/com/futureteachai/Function.java
+++ server/src/main/java/com/futureteachai/Function.java
@@ -1,5 +1,14 @@
 package com.futureteachai;
+import java.io.ByteArrayInputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
 
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -9,40 +18,45 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Optional;
-
-/**
- * Azure Functions with HTTP Trigger.
- */
 public class Function {
     @FunctionName("ProcessPdf")
     public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {
-                    HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<byte[]>> request,
+            @HttpTrigger(name = "req", methods = { HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<byte[]>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
         if (request.getBody().isPresent()) {
             try {
-                // Save the uploaded file to a temporary location
-                Path tempFilePath = Files.createTempFile("uploaded", ".pdf");
-                Files.write(tempFilePath, request.getBody().get());
+                String connectionString = System.getenv("AzureWebJobsStorage");
+                BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
+                
+                String containerName = "pdf-container";
+                BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
 
-                // Log the file path
-                context.getLogger().info("File saved to: " + tempFilePath.toString());
+                String baseBlobName = "file"; // base name
+                String uniqueBlobName = baseBlobName + "-" + generateUniqueIdentifier();
+                BlobClient blobClient = containerClient.getBlobClient(uniqueBlobName);
 
-                // TODO: Call document intelligence function with saved file
+                ByteArrayInputStream inputStream = new ByteArrayInputStream(request.getBody().get());
+                blobClient.upload(inputStream, request.getBody().get().length, true);
+                String blobUrl = String.format("https://%s.blob.core.windows.net/%s/%s",blobServiceClient.getAccountName(), containerName, uniqueBlobName);
 
-                return request.createResponseBuilder(HttpStatus.OK).body("File uploaded successfully").build();
-            } catch (IOException e) {
+                return request.createResponseBuilder(HttpStatus.OK).body(blobUrl).build();
+            } catch (Exception e) {
+                context.getLogger().severe("Error handling file upload: " + e.getMessage());
                 return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
                         .body("Error handling file upload").build();
             }
         } else {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please upload a file").build();
         }
+    }
+
+    private String generateUniqueIdentifier() {
+        // Using a timestamp and UUID to create a unique identifier
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd-HHmmssSSS");
+        String timestamp = dateFormat.format(new Date());
+        UUID uuid = UUID.randomUUID();
+        return timestamp + "-" + uuid.toString();
     }
 }
