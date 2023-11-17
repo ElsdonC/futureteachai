d904926c4e5e58c18ca54414a37b989ca0eb3f5c
Elsdon Chang <elsdonc21@gmail.com>
2023-11-16 21:42:21 -0600

    handle pdf upload

--- server/src/main/java/com/futureteachai/Function.java
+++ server/src/main/java/com/futureteachai/Function.java
@@ -9,35 +9,40 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
-    /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
-     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
-     */
-    @FunctionName("HttpExample")
+    @FunctionName("ProcessPdf")
     public HttpResponseMessage run(
-            @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.GET, HttpMethod.POST},
-                authLevel = AuthorizationLevel.ANONYMOUS)
-                HttpRequestMessage<Optional<String>> request,
+            @HttpTrigger(name = "req", methods = {
+                    HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<byte[]>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
-        // Parse query parameter
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
+        if (request.getBody().isPresent()) {
+            try {
+                // Save the uploaded file to a temporary location
+                Path tempFilePath = Files.createTempFile("uploaded", ".pdf");
+                Files.write(tempFilePath, request.getBody().get());
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+                // Log the file path
+                context.getLogger().info("File saved to: " + tempFilePath.toString());
+
+                // TODO: Call document intelligence function with saved file
+
+                return request.createResponseBuilder(HttpStatus.OK).body("File uploaded successfully").build();
+            } catch (IOException e) {
+                return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("Error handling file upload").build();
+            }
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please upload a file").build();
         }
     }
 }
