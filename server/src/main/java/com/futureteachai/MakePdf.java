e841e3244731ccd9e7eaa38556162112c2c45c20
cyb3r-anarch <aanon408@gmail.com>
2023-11-17 23:30:50 -0800

    change accepted format

--- server/src/main/java/com/futureteachai/MakePdf.java
+++ server/src/main/java/com/futureteachai/MakePdf.java
@@ -2,6 +2,7 @@ package com.futureteachai;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -13,27 +14,47 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
  */
 public class MakePdf {
     /**
-     * This function listens at endpoint "/api/MakePdf". Two ways to invoke it using
-     * "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/MakePdf
-     * 2. curl {your host}/api/MakePdf?name=HTTP%20Query
+     * This function listens at endpoint "/api/MakePdf".
+     * Invoke it using "curl" command in bash:
+     * curl -d "HTTP Body" {your host}/api/MakePdf
      */
     @FunctionName("MakePdf")
     public HttpResponseMessage run(
-        @HttpTrigger(name = "req", methods = {HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<String> request,
+        @HttpTrigger(
+            name = "req",
+            methods = {HttpMethod.POST},
+            authLevel = AuthorizationLevel.ANONYMOUS
+        ) HttpRequestMessage<String> request,
         final ExecutionContext context) {
-        
-        String[] questions = request.getBody().split(","); // Adjust based on your JSON format
 
-        byte[] pdfBytes = generatePdf(questions);
+        try {
+            // Get the questions from the request body
+            String questionsString = request.getBody();
 
-        return request.createResponseBuilder(HttpStatus.OK)
-                .header("Content-Type", "application/pdf")
-                .body(pdfBytes)
-                .build();
+            // Generate the PDF bytes
+            byte[] pdfBytes = generatePdf(questionsString);
+
+            // Return the PDF as the response
+            return request.createResponseBuilder(HttpStatus.OK)
+                    .header("Content-Type", "application/pdf")
+                    .body(pdfBytes)
+                    .build();
+        } catch (Exception e) {
+            // Handle exceptions and return an error response
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error generating PDF: " + e.getMessage())
+                    .build();
+        }
     }
 
-    public static byte[] generatePdf(String[] questions) {
+    /**
+     * Generates a PDF document from a string containing questions.
+     * @param questionsString The string containing questions.
+     * @return PDF document as byte array.
+     */
+    public static byte[] generatePdf(String questionsString) {
+        String[] questions = questionsString.split(","); // Adjust based on your JSON format
+
         try (PDDocument document = new PDDocument()) {
             PDPage page = new PDPage();
             document.addPage(page);
